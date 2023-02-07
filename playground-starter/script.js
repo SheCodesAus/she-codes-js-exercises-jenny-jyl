93e4b51d3bb0981d8165e7e0933cafe0c9d444bc
jenny-jyl <114784070+jenny-jyl@users.noreply.github.com>
2023-02-07 18:29:45 +1100

    update

--- playground-starter/script.js
+++ playground-starter/script.js
@@ -26,3 +26,27 @@ function showName() {
 }
 
 showName();
+
+// if statements
+if (pet == "Chilli") {
+    console.log("OMG you are the cutest!");
+} else if (pet == "Ivy") {
+    console.log("Naaaaw cute bow.");
+} else {
+    console.log("OMG so cute.");
+}
+
+// An array is a list of related items or information.
+let pets = ["Chilli", "Rex", "Roary", "Fleur"];
+console.log(pets);
+pets.push("Ivy");
+console.log(pets);
+console.log(pets[0]);
+console.log(pets[3]);
+
+// for loops
+// We can loop over items in an array using a standard for loop:
+for (let index = 0; index < pets.length; index++) { console.log(pets[index]); }
+
+// We can also loop over items in an array using forEach:
+pets.forEach((pet) => { console.log(pet); });
